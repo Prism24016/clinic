0f0e4a94b8714787f8d6169138b59828a908e4f6
Prism24016 <kimura.mika035@gmail.com>
2024-05-26 21:35:49 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,53 @@
+$(function () {
+  /*=================================================
+  ハンバーガーメニュー
+  ===================================================*/
+  $(".hamburger-btn").on("click", function () {
+    // headerにopenクラスがあるか判定する
+    if ($("header").hasClass("open")) {
+      // headerにopenクラスが存在しない場合、openクラスを削除する
+      $("header").removeClass("open");
+    } else {
+      // headerにopenクラスが存在する場合、openクラスを加える
+      $("header").addClass("open");
+    }
+  });
+
+  // メニューが表示されている時に画面をクリックした場合
+  $("#mask").on("click", function () {
+    $("header").removeClass("open");
+  });
+
+  // リンクをクリックした時にメニューを閉じる
+  $('.hamburger-menu a').on('click', function () {
+    $('#header').removeClass('open');
+  });
+
+  /*=================================================
+  トップに戻る
+  ===================================================*/
+  let pagetop = $(".to-top");
+  // 最初に画面が表示された時は、トップに戻るボタンを非表示に設定
+  pagetop.hide();
+
+  // スクロールイベント（スクロールされた際に実行）
+  $(window).scroll(function () {
+    // スクロール位置が300pxを超えた場合
+    if ($(this).scrollTop() > 300) {
+      // トップに戻るボタンを表示する
+      pagetop.fadeIn();
+
+      // スクロール位置が300px未満の場合
+    } else {
+      // トップに戻るボタンを非表示にする
+      pagetop.fadeOut();
+    }
+  });
+  // クリックイベント（ボタンがクリックされた際に実行）
+  pagetop.click(function () {
+    // 0.5秒かけてページトップへ移動
+    $("body,html").animate({ scrollTop: 0 }, 500);
+
+    return false;
+  });
+});
